c34558f745e60477823be340d3e99c97ad1b9117
lhy <984038622@qq.com>
2023-03-14 18:21:20 +0800

    init

---
+++ src/main/java/com/music/MusicApplication.java
@@ -0,0 +1,20 @@
+package com.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @name: MusicApplication
+ * @author: leihuangyan
+ * @classPath: com.music.MusicApplication
+ * @date: 2023/3/14
+ * @description: finished with non-zero exit value 1
+ */
+
+@SpringBootApplication
+public class MusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicApplication.class,args);
+    }
+}
---
+++ src/main/java/com/music/controller/IndexController.java
@@ -0,0 +1,24 @@
+package com.music.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @name: IndexController
+ * @author: leihuangyan
+ * @classPath: com.music.controller.IndexController
+ * @date: 2023/3/14
+ * @description:
+ */
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+
+
+    @GetMapping("/")
+    public String index(){
+        return "welcome to springboot";
+    }
+}
